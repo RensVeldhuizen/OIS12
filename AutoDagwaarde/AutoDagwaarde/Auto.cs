95f1cc241513226bd891aaee8ad4575aa55f3f81
RensVeldhuizen <rensveldhuizen@hotmail.com>
2019-06-14 15:16:06 +0200

    Add files via upload

---
+++ AutoDagwaarde/AutoDagwaarde/Auto.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoDagwaarde
+{
+    enum BrandstofSoort
+    {
+        Benzine = 100,
+        Diesel = 150,
+        LPG = 90,
+        Elektrisch = 130
+    }
+    class Auto
+    {
+        private int _KmStand = 0;
+        private string _Kenteken;
+        private int _Dagwaarde;
+        private BrandstofSoort _brandstof;
+
+        public void RijdKilometer(int gereden)
+        {
+            if (gereden > 0)
+            {
+                _KmStand = _KmStand + gereden;
+            }
+        }
+
+        public BrandstofSoort brandstof()
+        {
+            return _brandstof;
+        }
+
+        public Auto(string kenteken, BrandstofSoort brndstof, int kmStand)
+        {
+            _Kenteken = kenteken;
+            _brandstof = brndstof;
+            _brandstof++;
+            _KmStand = kmStand;
+        }
+
+        public int Dagwaarde()
+        {
+            _Dagwaarde = 500000 / _KmStand * (int)_brandstof;
+
+            return _Dagwaarde;
+        }
+
+        public int KmStand()
+        {
+            return _KmStand;
+        }
+
+        public override string ToString()
+        {
+            string data = String.Format("{0} {1} auto {2}km heeft een dagwaarde van {3}.", _Kenteken, _brandstof, _KmStand, _Dagwaarde);
+            return data;
+        }
+    }
+}
---
+++ AutoDagwaarde/AutoDagwaarde/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoDagwaarde
+{
+    public partial class Form1 : Form
+    {
+        List<Auto> autos = new List<Auto>()
+            {
+                new Auto("AB-123-C",BrandstofSoort.Benzine, 10000),
+                new Auto("AQ-123-A",BrandstofSoort.Diesel, 22000),
+                new Auto("AQ-673-A",BrandstofSoort.Elektrisch, 17000),
+                new Auto("AQ-5643-A",BrandstofSoort.LPG, 13000)
+            };
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            autos.Add(new Auto("",BrandstofSoort.Benzine,10000));
+            foreach (Auto auto in autos)
+            {
+                auto.Dagwaarde();
+                listBox1.Items.Add(auto.ToString());
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (Auto auto in autos)
+            {
+                if (auto.KmStand() < 100000)
+                {
+                    auto.RijdKilometer(rnd.Next(1000, 20000));
+                    auto.Dagwaarde();   
+                }
+                listBox1.Items.Add(auto.ToString());
+
+                BrandstofSoort brandstof = auto.brandstof();
+                brandstof++;
+                MessageBox.Show(brandstof.ToString());
+            }
+
+            
+        }
+    }
+}
